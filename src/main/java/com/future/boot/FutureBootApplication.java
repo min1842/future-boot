3d396a8eafaf1baca990fa6d6feff929c6b86233
Mr.Zhang <zhe1842@163.com>
2019-11-06 17:16:26 +0800

    init

---
+++ src/main/java/com/future/boot/FutureBootApplication.java
@@ -0,0 +1,13 @@
+package com.future.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/future/boot/FutureBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.future.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
